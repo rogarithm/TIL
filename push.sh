f7767b1ceb33861dd383005760b1a1f532d7d1aa
sehun-kim <rogarithm@gmail.com>
2022-09-28 11:27:51 +0900

    #3 바꾼 디렉토리 이름을 스크립트에 반영한다

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 # 예를 들어 2022년 9월 24일인 경우, 파일명은 20220924.md이고, 커밋 메시지는 20220924이다.
 # 스크립트가 있는 위치에서 ./push.sh 20220924를 실행하면 20220924.md 파일을 커밋하고 원격 저장소에 푸시한다.
 
-cd ~/study/TIL/Summary
+cd ~/study/TIL/2022-09
 git add $1.md &&
 git cm -m "$1" &&
 git push origin main
