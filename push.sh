9cd30e56a7df54f39b7a286d72636b1172f023e6
sehun-kim <rogarithm@gmail.com>
2022-10-02 17:23:42 +0900

    #3 이동할 디렉토리, 커밋할 파일 이름 명령어로 계산해서 자동 입력
     - push.sh의 인자로 연월일을 직접 치지 않고, today 또는 yesterday를
     인자로 주면 날짜를 자동으로 계산한다.
     - 두 조건문 안 중복이 있다. 나중에 중복 제거하기.
     - 디렉토리가 존재하지 않으면 만들어주는 로직을 넣으면 좋을 것 같다.

--- push.sh
+++ push.sh
@@ -2,8 +2,14 @@
 # $1: TIL 날짜.
 # 예를 들어 2022년 9월 24일인 경우, 파일명은 20220924.md이고, 커밋 메시지는 20220924이다.
 # 스크립트가 있는 위치에서 ./push.sh 20220924를 실행하면 20220924.md 파일을 커밋하고 원격 저장소에 푸시한다.
+cd ~/study/TIL/$(date "+%Y-%m") #2022-09
 
-cd ~/study/TIL/2022-09
-git add $1.md &&
-git cm -m "$1" &&
+if [[ $1 == 'today' ]]; then
+	git add $(date "+%Y%m%d").md &&
+		git cm -m "$(date "+%Y%m%d")"
+elif [[ $1 == 'yesterday' ]]; then
+	git add $(date -v -1d '+%Y%m%d').md &&
+		git cm -m "$(date -v -1d '+%Y%m%d')"
+fi
+
 git push origin main
