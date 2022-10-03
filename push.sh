be339a63b99eac5928f5492ffd91b78c178a7648
sehun-kim <rogarithm@gmail.com>
2022-10-04 02:02:30 +0900

    #3 리팩토링 후 테스트를 목적으로 git 명령 대신 echo로 대체

--- push.sh
+++ push.sh
@@ -2,14 +2,19 @@
 # $1: TIL 날짜.
 # 예를 들어 2022년 9월 24일인 경우, 파일명은 20220924.md이고, 커밋 메시지는 20220924이다.
 # 스크립트가 있는 위치에서 ./push.sh 20220924를 실행하면 20220924.md 파일을 커밋하고 원격 저장소에 푸시한다.
-cd ~/study/TIL/$(date "+%Y-%m") #2022-09
+dir_name=$(date "+%Y-%m")
+day=""
+echo ${dir_name}
+#cd ~/study/TIL/${dir_name} #2022-09
 
 if [[ $1 == 'today' ]]; then
-	git add $(date "+%Y%m%d").md &&
-		git cm -m "$(date "+%Y%m%d")"
+	day=$(date "+%Y%m%d")
+	echo ${day}
+	#git add ${day}.md && git cm -m "${day}"
 elif [[ $1 == 'yesterday' ]]; then
-	git add $(date -v -1d '+%Y%m%d').md &&
-		git cm -m "$(date -v -1d '+%Y%m%d')"
+	day=$(date -v -1d '+%Y%m%d')
+	echo ${day}
+	#git add ${day}.md && git cm -m "${day}"
 fi
 
-git push origin main
+#git push origin main
