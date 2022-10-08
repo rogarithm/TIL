6de69ec843c0e3e7f635265e9013f10fec26337b
sehun-kim <rogarithm@gmail.com>
2022-10-09 03:06:32 +0900

    #3 setup.sh 실행할 때 디렉토리 없으면 만들기

--- setup.sh
+++ setup.sh
@@ -9,10 +9,21 @@ dir_today=~/study/TIL/$(date "+%Y-%m")
 dir_yesterday=~/study/TIL/$(date -v -1d "+%Y-%m")
 today=$(date "+%Y%m%d")
 yesterday=$(date -v -1d '+%Y%m%d')
+
 if [[ $1 == 'today' ]]; then # 파일이 이미 있을 경우?
+	if [ ! -d ${dir_today} ]; then
+		mkdir ${dir_today} && cd ${dir_today}
+	else
+		cd ~/study/TIL/${dir_today} #2022-09
+	fi
 	touch ${dir_today}/${today}.md &&
 		vim ${dir_today}/${today}.md # til 문서를 바로 편집하는 경우가 대부분인데 파일 이름을 확인하고 입력하는과정이 번거롭다. 바로 파일을 편집하자.
 elif [[ $1 == 'yesterday' ]]; then
+	if [ ! -d ${dir_yesterday} ]; then
+		mkdir ${dir_yesterday} && cd ${dir_yesterday}
+	else
+		cd ${dir_yesterday} #2022-09
+	fi
 	touch ${dir_yesterday}/${yesterday}.md &&
 		vim ${dir_yesterday}/${yesterday}.md
 fi
