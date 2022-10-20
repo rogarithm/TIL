d1f3c80ab2c3354f00d4f311d1df48598081d6ae
sehun-kim <rogarithm@gmail.com>
2022-10-20 15:17:38 +0900

    [#8] sed로 빈칸 문자 교체 시도. 실패

--- push.sh
+++ push.sh
@@ -14,6 +14,8 @@ case "$char"
 	-t | --today) day=$(date "+%Y%m%d");;
 esac
 
+sed -i -e 's/^     /  /g' ${day}.md # 자동 줄바꿈시 들어가는 탭 문자를 빈칸 두 개로 바꾼다.
+
 echo "ADDING TIL OF " ${day}
 git add ${day}.md && git cm -m "${day}"
 
