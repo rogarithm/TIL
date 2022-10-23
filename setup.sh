c46159616f905b1c887ac25119af65049435a122
sehun-kim <rogarithm@gmail.com>
2022-10-23 23:04:48 +0900

    [#3] 스크립트 중복 제거, 숫자를 인자로 파일 만들 수 있게 변경

--- setup.sh
+++ setup.sh
@@ -4,27 +4,34 @@
 # ex1. ./setup.sh yesterday -> makes 2022-10/20221002.md (if yesterday is 2022-10-02)
 
 # 디렉토리가 이미 있으면 파일만 만들고, 아니면 먼저 디렉토리를 만든다
+arg=$1
 
-dir_today=~/study/TIL/$(date "+%Y-%m")
-dir_yesterday=~/study/TIL/$(date -v -1d "+%Y-%m")
-today=$(date "+%Y%m%d")
-yesterday=$(date -v -1d '+%Y%m%d')
-
-char=$1
-case "$char" in
+case "$arg" in
+	-d) shift
+		arg=$1
+		echo $arg
+		;;
 	-y | --yesterday) 
-		if [ ! -d ${dir_yesterday} ]; then
-			mkdir ${dir_yesterday} && cd ${dir_yesterday}
-		else
-			cd ${dir_yesterday}
-		fi
-		touch ${dir_yesterday}/${yesterday}.md && vim ${dir_yesterday}/${yesterday}.md;;
+		arg='-1'
+		echo $arg
+		;;
 	-t | --today)
-		if [ ! -d ${dir_today} ]; then
-			mkdir ${dir_today} && cd ${dir_today}
-		else
-			cd ~/study/TIL/${dir_today}
-		fi
-		# til 문서를 바로 편집하는 경우가 대부분인데 파일 이름을 확인하고 입력하는과정이 번거롭다. 바로 파일을 편집하자.
-		touch ${dir_today}/${today}.md && vim ${dir_today}/${today}.md;;
+		arg='-0'
+		echo $arg
+		;;
 esac
+
+til_dir=~/study/TIL/$(date -v ${arg}d "+%Y-%m")
+til_file_prefix=$(date -v -0d "+%Y%m%d")
+
+echo "setting up til " ${arg} "day from today: " $(date -v ${arg}d "+%Y-%m-%d") >&2
+echo "directory is " ${til_dir} >&2
+echo "and file prefix is " ${til_file_prefix} >&2
+
+if [ ! -d ${til_dir} ]; then
+	mkdir ${til_dir} && cd ${til_dir}
+else
+	cd ${til_dir}
+fi
+# til 문서를 바로 편집하는 경우가 대부분인데 파일 이름을 확인하고 입력하는과정이 번거롭다. 바로 파일을 편집하자.
+touch ${til_dir}/${til_file_prefix}.md && vim ${til_dir}/${til_file_prefix}.md
